95ef6ae012e547b20fd87ddc518e91db1c999f82
Mathia haure-Touzé <mathia.haure-touze@owkin.com>
2025-07-09 15:41:56 +0000

    install dev tools

--- startup.sh
+++ startup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 # install tools
-sudo apt install --yes \
-    direnv
+sudo apt update \ 
+    && apt install --no-install-recommends --yes \
+        direnv \
+        pkg-config \
+        tig \
+        zsh \
+    && rm -rf /var/lib/apt/lists/*
 
-# install zsh, of my zsh and theme
-sudo apt install zsh --yes
-# Install 
+# install lxml depenencies
+sudo apt update \
+    && apt install --no-install-recommends --yes \
+        libxml2-dev \
+        libxmlsec1-dev \
+        libxmlsec1-openssl \
+    && rm -rf /var/lib/apt/lists/*
+
+# customize zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --yes
 sudo chsh -s $(which zsh)
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
